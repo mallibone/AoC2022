2230e3d07810e32d7d5713e2d596142141eb312b
Mark Allibone <mark.allibone@rey-technology.com>
2022-12-03 08:33:58 +0100

    Solve day 3

--- AdventOfCode2022/Day03.fs
+++ AdventOfCode2022/Day03.fs
@@ -1,10 +1,50 @@
 module Day03
 
+// Shared
+let scoreCard = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray() |> Array.mapi (fun i v -> v, i+1)
+
+let toPriority item =
+    scoreCard |> Seq.find (fun (code, value) -> code = item) |> snd
+
+// Part 1
+let parsePart1 (rucksackContents:string) =
+    let middle = rucksackContents.Length / 2
+    rucksackContents.Substring(0,middle), rucksackContents.Substring(middle, middle)
+
+let findDuplicates (firstCompartment:string, secondCompartment:string) =
+    let itemsOfFirstCompartment = firstCompartment.ToCharArray()
+    let itemsOfSecondCompartment = secondCompartment.ToCharArray()
+    itemsOfFirstCompartment |> Seq.find (fun i -> itemsOfSecondCompartment |> Seq.contains i)
+
+// Part 2
+let makeGroups backpacks =
+    let rec groupMaker result remainingBackpacks =
+        match remainingBackpacks with
+        | [] -> result
+        | _ -> groupMaker (result@[(remainingBackpacks |> List.take 3)]) (remainingBackpacks |> List.skip 3)
+    groupMaker [] (backpacks |> Array.toList)
+
+let getGroupBadge (groupPacks:list<string>) =
+    let itemsFirstPack = groupPacks[0].ToCharArray()
+    let itemsSecondPack = groupPacks[1].ToCharArray()
+    let itemsThirdPack = groupPacks[2].ToCharArray()
+
+    let overlapingItems = itemsFirstPack |> Seq.filter (fun i -> itemsSecondPack |> Seq.contains i)
+    itemsThirdPack |> Seq.find (fun i -> overlapingItems |> Seq.contains i)
+
 let part1 input =
     input
-    
+    |> Seq.map parsePart1
+    |> Seq.map findDuplicates
+    |> Seq.map toPriority
+    |> Seq.sum
+
 let part2 input =
     input
+    |> makeGroups
+    |> List.map getGroupBadge
+    |> Seq.map toPriority
+    |> Seq.sum
     
 let executeDay (testInput:string[]) (input:string[]) =
     // part 1
--- AdventOfCode2022/Scratchpad.fsx
+++ AdventOfCode2022/Scratchpad.fsx
@@ -14,13 +14,49 @@ let getInput day =
     let filename day = Path.Combine(__SOURCE_DIRECTORY__, $"Input/Day{day}.txt")
     File.ReadAllLines(filename day)
 
+// Part 1
+let parsePart1 (rucksackContents:string) =
+    let middle = rucksackContents.Length / 2
+    rucksackContents.Substring(0,middle), rucksackContents.Substring(middle, middle)
 
+let findDuplicates (firstCompartment:string, secondCompartment:string) =
+    let itemsOfFirstCompartment = firstCompartment.ToCharArray()
+    let itemsOfSecondCompartment = secondCompartment.ToCharArray()
+    itemsOfFirstCompartment |> Seq.find (fun i -> itemsOfSecondCompartment |> Seq.contains i)
+
+let scoreCard = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray() |> Array.mapi (fun i v -> v, i+1)
+
+let toPriority item =
+    scoreCard |> Seq.find (fun (code, value) -> code = item) |> snd
+
+// Part 2
+let makeGroups backpacks =
+    let rec groupMaker result remainingBackpacks =
+        match remainingBackpacks with
+        | [] -> result
+        | _ -> groupMaker (result@[(remainingBackpacks |> List.take 3)]) (remainingBackpacks |> List.skip 3)
+    groupMaker [] (backpacks |> Array.toList)
+
+let getGroupBadge (groupPacks:list<string>) =
+    let itemsFirstPack = groupPacks[0].ToCharArray()
+    let itemsSecondPack = groupPacks[1].ToCharArray()
+    let itemsThirdPack = groupPacks[2].ToCharArray()
+
+    let overlapingItems = itemsFirstPack |> Seq.filter (fun i -> itemsSecondPack |> Seq.contains i)
+    itemsThirdPack |> Seq.find (fun i -> overlapingItems |> Seq.contains i)
 
 // part 1
-getInput 2
-// getTestInput 2
-
+getInput 3
+// getTestInput 3
+|> Seq.map parsePart1
+|> Seq.map findDuplicates
+|> Seq.map toPriority
+|> Seq.sum
 
 // part 2
-getInput 2
-// getTestInput 2
+getInput 3
+// getTestInput 3
+|> makeGroups
+|> List.map getGroupBadge
+|> Seq.map toPriority
+|> Seq.sum
