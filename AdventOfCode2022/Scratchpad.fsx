b64f8a88396922672e50de8fd3f8a12064e6dd94
Mark Allibone <mark.allibone@rey-technology.com>
2022-12-13 11:13:18 +0100

    WIP day 13

--- AdventOfCode2022/Scratchpad.fsx
+++ AdventOfCode2022/Scratchpad.fsx
@@ -10,19 +10,48 @@ open System.Collections.Concurrent
 
 let getTestInput (day:int) =
     let filename = Path.Combine(__SOURCE_DIRECTORY__, $"Input/TestDay{day}.txt")
-    File.ReadAllLines(filename)
-    // File.ReadAllText(filename)
+    // File.ReadAllLines(filename)
+    File.ReadAllText(filename)
 
 let getInput (day:int) =
     let filename = Path.Combine(__SOURCE_DIRECTORY__, $"Input/Day{day}.txt")
-    File.ReadAllLines(filename)
-    // File.ReadAllText(filename)
+    // File.ReadAllLines(filename)
+    File.ReadAllText(filename)
 
+type PaketElement =
+    | Number of int
+    | SubPaket of list<PaketElement>
+
+let rec parsePaket paketInput =
+    match paketInput with
+    | "[]" -> []
+    | _ ->
+        // remove braces
+        let innerPaketInput = paketInput.Substring(1,paketInput.Length-2)
+        let innerPaketElements =
+            if innerPaketInput.Contains("[") then
+                // this is wrong -> need to take first [ then step into subparser, exit subparser if same level reaches ]
+                let innerSubPaketInput = paketInput.Substring(innerPaketInput.IndexOf("["),innerPaketInput.LastIndexOf("]"))
+                printfn "%s" innerSubPaketInput
+                innerPaketInput.Replace(innerSubPaketInput, "{gnabber}")
+            else
+                innerPaketInput
+
+        [for paketElement in innerPaketElements.Split(",") do 
+            if paketElement = "{gnabber}" then
+                yield SubPaket (parsePaket paketElement)
+            else
+                yield Number (int paketElement)]
+
+let parseInput (packetInput:string) =
+    packetInput.Split(System.Environment.NewLine + "" + System.Environment.NewLine)
+    |> Array.map (fun pairInput -> pairInput.Split(System.Environment.NewLine) |> Array.map parsePaket)
 
 // part 1
 // let gna = 
 // getInput 13
 getTestInput 13
+|> parseInput
 
 // part 2
 // getInput 13
